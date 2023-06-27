89a0fd1e1cef53449ab8ef5ea7e5484347cade34
hwangdae <klp2797@naver.com>
2023-06-27 16:44:23 +0900

    -피드 부분 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,16 +3,21 @@ import SignUp from "../components/SignUp";
 import TopBar from "../components/TopBar";
 import SideBar from "../components/SideBar";
 import SignIn from "../components/SignIn";
+import Contents from "../components/Contents";
 
 
 function App() {
   return (
-    <>
+    <div style={{
+      maxWidth : "1200px",
+      margin : "0 auto"
+    }}>
       <TopBar />
       <SideBar />
       <SignIn />
-      <SignUp />;
-    </>
+      <SignUp />
+      <Contents/>;
+    </div>
   );
 }
 
