37575d077c7c9511e2623a76dc91b2bc0b21dc64
YuGil <yugil3150@naver.com>
2023-06-27 16:34:00 +0900

    Merge With Social LogIn

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-// import SignUp from "../components/SignUp";
 import TopBar from "../components/TopBar";
 import SideBar from "../components/SideBar";
-// import SignIn from "../components/SignIn";
 
 function App() {
   return (
     <>
       <TopBar />
       <SideBar />
-      {/* <SignIn />
-      <SignUp />; */}
     </>
   );
 }
