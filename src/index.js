24b69f8acc0d1b22b19e1860dda1c4a53d3ebf35
YuGil <yugil3150@naver.com>
2023-06-27 16:31:20 +0900

    Merge With Social Login

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./config/configStore";
-import app from "./service/firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -6,7 +6,7 @@ import {
   GoogleAuthProvider,
   GithubAuthProvider,
 } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
